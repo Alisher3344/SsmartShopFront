2842be0a4d773194fc2e021998814ee178755fcd
tty0x <tty0x@server-box2.tailbed877.ts.net>
2026-05-05 03:36:37 +0500

    fix(admin): set vite base to /punktadmin/ for nginx proxy

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/admin/',
+  base: '/punktadmin/',
   plugins: [react()],
   cacheDir: 'node_modules/.vite-admin',
   clearScreen: false,
